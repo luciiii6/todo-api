880208fefc4eff37ef929cb9a197dcc94272c1ad
Luci <luci.suvaila@gmail.com>
2022-07-04 13:08:11 +0300

    Update database table

--- db/migrate/20220704072239_create_todos.rb
+++ db/migrate/20220704072239_create_todos.rb
@@ -1,7 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[7.0]
   def change
     create_table :todos do |t|
-      t.string :content
+      t.string :content, null: false
       t.boolean :completed
 
       t.timestamps
---
+++ db/migrate/20220704091321_validate_content.rb
@@ -0,0 +1,5 @@
+class ValidateContent < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :todos, :content, null: false
+  end
+end
