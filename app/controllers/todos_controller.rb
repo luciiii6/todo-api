406af44896cf904cadd40de5745a08d7e01fba48
Luci <luci.suvaila@gmail.com>
2022-07-04 16:34:21 +0300

    Implement /update endpoint along with the tests

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 class TodosController < ApplicationController
   def create
-    todo = Todo.create(content: validated_params[:content], completed: false)
-    render action: "index", status: :created
+    Todo.create(content: validated_params[:content], completed: false)
+    render action: 'index', status: :created
   rescue ActionController::ParameterMissing
-    render json: { error: "Content missing" }, status: 400
+    render json: { error: 'Content missing' }, status: 400
   end
 
   def index
     todos = Todo.all
 
-    render json: {todos: todos}, status: :ok
+    render json: { todos: todos }, status: :ok
   end
 
   def update
-    pp params[:id]
-    todo = Todo.find_by(id: params[:id])
+    todo = Todo.find_by!(id: params[:id])
     todo.content = validated_params[:content]
     todo.completed = validated_params[:completed]
     todo.save!
 
-    render action: "index", status: :ok
-  rescue
-    render json: { error: "Todo not found" }, status: 404
+    render action: 'index', status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Todo not found' }, status: 404
+  rescue ActionController::ParameterMissing
+    render json: { error: 'Todo not found' }, status: 400
   end
 
   private
@@ -31,9 +34,9 @@ class TodosController < ApplicationController
   end
 
   def validated_params
-    return todo_params unless todo_params[:content] == "" &&
-                              todo_params[:completed] != true &&
-                              todo_params[:completed] != false
-    raise ActionController::ParameterMissing.new("Parameter content is an empty string")
+    return todo_params if todo_params.key?('content') && todo_params['content'] != '' && !todo_params.key?('completed')
+    return todo_params if todo_params['completed'] == 'true' || todo_params['completed'] == 'false'
+
+    raise ActionController::ParameterMissing, 'Wrong parameters for request'
   end
 end
