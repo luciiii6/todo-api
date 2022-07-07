ea6fd96b95afa1044790034068f8ce7dc50e13af
Luci <luci.suvaila@gmail.com>
2022-07-07 11:55:19 +0300

    Apply rubocop changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -8,9 +9,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins '*'
 
-    resource "*",
+    resource '*',
              headers: :any,
              methods: any
   end
