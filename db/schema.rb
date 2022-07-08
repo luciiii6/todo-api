81073ea0e59ddb7b4ddbb9d7c7b4e43f8b1e2ab4
Luci <luci.suvaila@gmail.com>
2022-07-08 10:42:51 +0300

    Apply rubocop changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,17 +12,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_08_071845) do
+ActiveRecord::Schema[7.0].define(version: 20_220_708_071_845) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "todos", force: :cascade do |t|
-    t.boolean "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "title", null: false
-    t.string "url"
-    t.integer "order"
+  create_table 'todos', force: :cascade do |t|
+    t.boolean 'completed'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'title', null: false
+    t.string 'url'
+    t.integer 'order'
   end
-
 end
