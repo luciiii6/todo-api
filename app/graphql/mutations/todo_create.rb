1dd5e6265b831ccd446dd8e110db7ad0594dac09
Luci <luci.suvaila@gmail.com>
2022-07-13 16:43:32 +0300

    Apply rubocop changes

--- app/graphql/mutations/todo_create.rb
+++ app/graphql/mutations/todo_create.rb
@@ -2,7 +2,7 @@
 
 module Mutations
   class TodoCreate < BaseMutation
-    description "Creates a new todo"
+    description 'Creates a new todo'
 
     field :todo, Types::TodoType, null: false
 
@@ -11,8 +11,9 @@ module Mutations
     def resolve(todo_input:)
       todo = Todo.create(**todo_input)
 
-      raise GraphQL::ExecutionError.new "Error creating todo", extensions: todo.errors.to_hash unless todo.save
-      todo.url = "test"
+      raise GraphQL::ExecutionError.new 'Error creating todo', extensions: todo.errors.to_hash unless todo.save
+
+      todo.url = 'test'
       todo.save
 
       { todo: todo }
