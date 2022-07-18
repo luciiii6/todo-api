5b33bdad017a2bdc3e349620b05b1a63fba2b866
Luci <luci.suvaila@gmail.com>
2022-07-18 16:05:00 +0300

    Change name of validator class

--- app/helpers/todo_validator.rb
+++ app/helpers/todo_validator.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-class Validator
+class TodoValidator
   class << self
-    def validated_params_for_create(params)
+    def validate_params_for_create(params)
       return params if params.key?('title') && params['title'] != '' && !params.key?('completed')
       return params if params['completed'] == true || params['completed'] == false
 
       raise ActionController::ParameterMissing, 'Wrong parameters for request'
     end
 
-    def validated_params_for_update(params)
+    def validate_params_for_update(params)
       if params.empty? || params['completed'].is_a?(String)
         raise ActionController::ParameterMissing,
               'Wrong parameters for request'
