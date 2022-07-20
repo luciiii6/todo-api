adcca4349e7696622e906825278e50fcdaa5502b
Luci <luci.suvaila@gmail.com>
2022-07-20 14:49:25 +0300

    Add validation for sort_by field

--- app/helpers/page_parameters_validator.rb
+++ app/helpers/page_parameters_validator.rb
@@ -6,7 +6,7 @@ class PageParametersValidator
 
   class << self
     def valid?(parameters)
-      if parameters.key?('before') && parameters.key?('after')
+      if (parameters.key?('before') && parameters.key?('after')) || !Todo.has_attribute?(parameters['sort_by'])
         raise PageError, "Can't have before and after in the same request"
       end
 
