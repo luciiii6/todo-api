19f45bfaddf45cee5bfeccd06d0b8e2ed75d260d
Luci <luci.suvaila@gmail.com>
2022-07-20 14:50:43 +0300

    Change the default scope of the model with another scope

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
-  default_scope { order(created_at: :desc) }
+  scope :sorted_by, ->(attribute, direction = 'DESC') { order("#{attribute} #{direction}") }
 end
